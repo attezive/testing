50be02f77b5b25d713b0beaa278c287672e0b1c0
Attezive <attezive@gmail.com>
2024-10-19 04:16:29 +0300

    itns
    y

--- firsttask/__init__.py
+++ firsttask/__init__.py
@@ -5,3 +5,17 @@ print(np.zeros(10))
 
 data = sb.load_dataset("flights")
 print(data.head())
+
+
+for i in "Hellow world":
+    print(i, end=" ")
+
+print("Hellow world")
+# Какие-то комментарии
+
+
+c = "2"
+d = "3"
+
+print(c + d)
+print(c * 10)
